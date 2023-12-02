1e911f9e52d884200383390a26b8841e8998cd34
imran0dot <detectiveseo1@gmail.com>
2023-12-02 17:14:57 +0600

    header design complete

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header id="header">
-            <div className="container">
+        <header id="header" className='bg-black py-4 bg-opacity-50'>
+            <div className="container flex justify-between">
                 <Image src={logo} alt='logo' />
-                <div className="flex items-center">
-                    <a className="text-white min-w-[50px] font-medium" href="#">Home</a>
-                    <button className="log-btn btn">Login</button>
+                <div className="flex items-center text-white">
+                    <a className="min-w-[50px] font-medium" href="#">Home</a>
+                    <button className="log-btn btn bg-green-300 px-3 py-1 font-bold text-black rounded-md">Login</button>
                 </div>
             </div>
         </header>
